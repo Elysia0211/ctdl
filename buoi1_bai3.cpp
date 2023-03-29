be4c7d9fd4d145022a59124dcdf3a07a5d67670c
Elysia0211 <124846387+Elysia0211@users.noreply.github.com>
2023-03-29 16:25:49 +0700

    Update buoi1_bai3.cpp

--- buoi1_bai3.cpp
+++ buoi1_bai3.cpp
@@ -3,22 +3,23 @@ using namespace std;
 long tinhXn(int);
 long tinhYn(int);
 long tinhXn(int n){
- if(n==0){
-  return 1;
-  return tinhXn(n-1) + tinhYn(n-1);
-  }
+    if(n==0){
+        return 1;
+    }
+    return tinhXn(n-1) + tinhYn(n-1);
+}
 long tinhYn(int n){
- if(n==0){
-  return 1;
-  return 3*tinhXn(n-1) + 2*tinhYn(n-1);
-  }
-int main()
-{
+    if(n==0){
+        return 0;
+    }
+    return 3*tinhXn(n-1) + 2*tinhYn(n-1);
+}
+int main(){
     int n;
     long S1, S2;
-    cout<<"\nNhap n: ";
+    cout<<"Nhap n: ";
     cin>>n;
-    long S1=tinhXn(n);
-    long S2(n)=tinhYn(n);
-    cout<<"\nXn = "<<%d<<"\va Yn = "<<%d<<S1,S2<<endl;
+    S1=tinhXn(n);
+    S2=tinhYn(n);
+    cout<<"\nXn = "<<S1<<"\tva Yn = "<<S2;
 }
